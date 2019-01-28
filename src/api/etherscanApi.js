a9ed07ec7f51ff670ca312479fa48d644250650a
BoleMerkle <jedibojan@gmail.com>
2019-01-28 17:45:27 +0100

    getTokenBalance implemented

--- src/api/etherscanApi.js
+++ src/api/etherscanApi.js
@@ -1,18 +1,34 @@
+import web3 from "web3";
+
 const api = require("etherscan-api").init("Y79W58GXJEW8KYIEMU39AGZ2U44KCJS2ZS");
 const tokenContractAddress = "0xbd149d3f7614b52f8a5e7e8986afd77e5d0e57cf";
 
 export function getTokenBalance(address) {
   return new Promise(accept => {
-    let balance = api.account.tokenbalance(address, "", tokenContractAddress);
-    balance.then(returnValue => {
-      accept(returnValue.result / 1000000000000000000);
-      return;
+    let paramData = web3.utils.sha3("balanceOf(address)").substring(0, 10);
+    paramData =
+      paramData +
+      "000000000000000000000000" +
+      address.substring(2, address.length);
+    let test = api.proxy.eth_call(tokenContractAddress, paramData, "latest");
+    test.then(returnValue => {
+      accept(parseInt(returnValue.result) / 1000000000000000000);
     });
   });
 }
 
 export function getTxHistory(address) {
   return new Promise(accept => {
+    var txList = api.account.tokentx(
+      address,
+      tokenContractAddress,
+      1,
+      "latest",
+      "asc"
+    );
+    txList.then(returnValue => {
+      console.log(returnValue);
+    });
     if (address) {
       const result = [
         [
@@ -20,7 +36,7 @@ export function getTxHistory(address) {
           "0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef",
           "0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef",
           "3",
-          "pending"
+          "locked"
         ]
       ];
       accept(result);
